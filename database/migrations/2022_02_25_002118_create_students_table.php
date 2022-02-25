4d6ef300472187f0281faeee7d6af514725877f4
EDuardo Gomez <juan.gomez@cceo.com.mx>
2022-02-24 18:30:53 -0600

    Was created migration students and added a register in table

---
+++ database/migrations/2022_02_25_002118_create_students_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->integer('control')->unique();
+            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone')->unique()->nullable();
+            $table->string('email')->unique();
+            $table->string('semester');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
